f79b081e5d3a63dee83ac2505627e6e460f346d8
NabillaCherylFauzani <lbil07119@gmail.com>
2025-06-27 18:46:48 +0700

    Add files via upload

---
+++ Taksi.java
@@ -0,0 +1,45 @@
+package TugasLanjutan;
+
+public class Taksi extends Mobil {
+    protected int tarifAwal;
+    protected int tarifPerKm;
+
+    public Taksi() {
+        super();
+    }
+
+    public Taksi(int tarifAwal, int tarifPerKm) {
+        super();
+        this.tarifAwal = tarifAwal;
+        this.tarifPerKm = tarifPerKm;
+    }
+
+    public Taksi(int jmlRoda, String warna, String bahanBakar, int kapasitasMesin, int tarifAwal, int tarifPerKm) {
+        super(jmlRoda, warna, bahanBakar, kapasitasMesin);
+        this.tarifAwal = tarifAwal;
+        this.tarifPerKm = tarifPerKm;
+    }
+
+    public void setTarifAwal(int tarifAwal) {
+        this.tarifAwal = tarifAwal;
+    }
+
+    public int getTarifAwal() {
+        return tarifAwal;
+    }
+
+    public void setTarifPerKm(int tarifPerKm) {
+        this.tarifPerKm = tarifPerKm;
+    }
+
+    public int getTarifPerKm() {
+        return tarifPerKm;
+    }
+
+    @Override
+    public void cetak() {
+        super.cetak();
+        System.out.println("Tarif awal             : Rp" + this.tarifAwal);
+        System.out.println("Tarif per Km           : Rp" + this.tarifPerKm);
+    }
+}
